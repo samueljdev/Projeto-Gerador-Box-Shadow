0185d8ba8d432ca71813391ede3cdb5f882f1867
samueljdev <samuel.jdev11@gmail.com>
2025-07-28 01:19:30 -0400

    Concluindo eventos

--- js/scritps.js
+++ js/scritps.js
@@ -55,6 +55,15 @@ class BoxShadowGenerator {
             case "horizontal":
                 this.horizontalRef.value = value;
                 break;
+            case "vertical":
+                this.verticalRef.value = value;
+                break;
+            case "blur":
+                this.blurRef.value = value;
+                break;
+            case "spread":
+                this.spreadRef.value = value;
+                break;
         }
 
         this.applyRule();
@@ -99,4 +108,22 @@ horizontal.addEventListener("input", (e) => {
     const value = e.target.value;
 
     boxShadow.updateValue("horizontal", value);
+});
+
+vertical.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    boxShadow.updateValue("vertical", value);
+});
+
+blur.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    boxShadow.updateValue("blur", value);
+});
+
+spread.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    boxShadow.updateValue("spread", value);
 });
